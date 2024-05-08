77ba4e7ea6e89c3c4e3ac29dc5ac3a17aa0c39d6
zkyzx <zkyzx@zkyzx.net>
2024-05-08 00:28:05 -0400

    pass meta attributes to buildNimPackage

--- buildNimblePackage.nix
+++ buildNimblePackage.nix
@@ -1,23 +1,37 @@
-{pkgs}: {
+{ pkgs }:
+{
   pname,
   version,
   name ? "${pname}-${version}",
   src,
   deps ? src + "/nimble2nix.json",
-  buildInputs ? [],
-  nativeBuildInputs ? [],
-  nimFlags ? [],
+  buildInputs ? [ ],
+  nativeBuildInputs ? [ ],
+  nimFlags ? [ ],
   nimRelease ? true,
-  nimDefines ? [],
+  nimDefines ? [ ],
   postInstall ? '''',
+  postBuild ? '''',
+  meta ? { },
 }:
 pkgs.buildNimPackage {
-  inherit name src nativeBuildInputs nimFlags nimRelease nimDefines postInstall;
+  inherit
+    name
+    src
+    nativeBuildInputs
+    nimFlags
+    nimRelease
+    nimDefines
+    postInstall
+    postBuild
+    meta
+    ;
   buildInputs =
     buildInputs
-    ++ (pkgs.lib.mapAttrsToList
-      (name: src:
-        pkgs.fetchgit {
-          inherit (src) url rev sha256 fetchSubmodules;
-        }) (pkgs.lib.importJSON deps));
+    ++ (pkgs.lib.mapAttrsToList (
+      _: src:
+      pkgs.fetchgit {
+        inherit (src) url rev sha256 fetchSubmodules;
+      }
+    ) (pkgs.lib.importJSON deps));
 }
